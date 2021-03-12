067f3bfc2b78b465ef4af5ac635c59ae8af90c74
ElectroluxV2 <electroluxv2@gmail.com>
2021-03-12 23:01:50 +0100

    Prefer pushd to cd

--- export.sh
+++ export.sh
@@ -1,29 +1,31 @@
-#!/usr/bin/env bash
+#!/bin/bash
 pushd /home/ubuntu/skiff-builder || return 1
 
 . ./variables.sh
 
 if [ -d 'build' ]; then
 
+ls
+
+  pushd build || (popd && exit 2)
   git pull origin main
 
 else
 
+  pushd build || (popd && exit 3)
   git clone $repository 'build'
 
 fi
-
-pushd 'build' || popd && exit 2
 
 export PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true
 export PUPPETEER_EXECUTABLE_PATH='/snap/bin/chromium'
 
 npm i
-
 npm run prod
 
 lftp -u $user,$password $server -e "set ftp:ssl-allow no; mirror -R $local $remote; exit"
 
-popd || exit 3
+popd || exit 4
+popd || exit 5
 
 exit 0
